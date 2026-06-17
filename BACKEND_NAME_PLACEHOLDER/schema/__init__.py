eccb2adcaf60f7c15e28ac48c9520a66ab3dabed
lostexee <leonsampl10@gmail.com>
2026-06-17 09:27:50 +0200

    commit

--- BACKEND_NAME_PLACEHOLDER/schema/__init__.py
+++ BACKEND_NAME_PLACEHOLDER/schema/__init__.py
@@ -1,11 +1,17 @@
 from ._entity import EntityBase, EntityFilter, EntityFull
-from ._user import UserBase, UserFilter, UserFull
+from ._token import LoginRequest, Token, TokenData
+from ._user import UserBase, UserCreate, UserFilter, UserFull, UserPublic
 
 __all__ = [
     "EntityBase",
     "EntityFilter",
     "EntityFull",
+    "LoginRequest",
+    "Token",
+    "TokenData",
     "UserBase",
-    "UserFull",
+    "UserCreate",
     "UserFilter",
+    "UserFull",
+    "UserPublic",
 ]
